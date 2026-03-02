ba8d466ff777f88e167936d308dfa1bdafc2a0c0
chihyinwang <a36980097@gmail.com>
2026-03-02 10:57:08 +0000

    Initial Commit

---
+++ AXD Installation/AXD_InstallationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXD_InstallationApp.swift
+//  AXD Installation
+//
+//  Created by chihyin wang on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct AXD_InstallationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
